3cbcb67ba1ec875f258d85bf0bd0be5ef645ec86
github-action-benchmark <github@users.noreply.github.com>
2021-07-21 15:03:34 +0000

    add Benchmark (googlecpp) benchmark result for ee45556de7764d27102d7e6aec5624282fe31de1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1626879423894,
+  "lastUpdate": 1626879814289,
   "repoUrl": "https://github.com/ZigRazor/CXXGraph",
   "entries": {
     "Benchmark": [
@@ -78,6 +78,84 @@ window.BENCHMARK_DATA = {
             "value": 1.6737624341602153,
             "unit": "ns/iter",
             "extra": "iterations: 418217527\ncpu: 1.6736113285849925 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zigrazor@gmail.com",
+            "name": "ZigRazor",
+            "username": "ZigRazor"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ee45556de7764d27102d7e6aec5624282fe31de1",
+          "message": "Update README.md",
+          "timestamp": "2021-07-21T17:00:40+02:00",
+          "tree_id": "44c754622d72bd5f977d42fbf2e7474a8582e1bc",
+          "url": "https://github.com/ZigRazor/CXXGraph/commit/ee45556de7764d27102d7e6aec5624282fe31de1"
+        },
+        "date": 1626879813418,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_NodeCreation",
+            "value": 5.999999928008037e-7,
+            "unit": "ns/iter",
+            "extra": "iterations: 1000000000\ncpu: 7.000000000001796e-7 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_NodeGetId",
+            "value": 6.999999868639861e-7,
+            "unit": "ns/iter",
+            "extra": "iterations: 1000000000\ncpu: 7.000000000001796e-7 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_NodeGetId",
+            "value": 5.999999928008037e-7,
+            "unit": "ns/iter",
+            "extra": "iterations: 1000000000\ncpu: 6.999999999997459e-7 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_EdgeCreation_1",
+            "value": 6.999999868639861e-7,
+            "unit": "ns/iter",
+            "extra": "iterations: 1000000000\ncpu: 6.999999999997459e-7 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_EdgeCreationDestruction_2",
+            "value": 15.351707788154835,
+            "unit": "ns/iter",
+            "extra": "iterations: 45735942\ncpu: 15.283770497172661 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_EdgeGetId",
+            "value": 5.999999928008037e-7,
+            "unit": "ns/iter",
+            "extra": "iterations: 1000000000\ncpu: 7.000000000756402e-7 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_NodeGetNodePair",
+            "value": 6.999999868639861e-7,
+            "unit": "ns/iter",
+            "extra": "iterations: 1000000000\ncpu: 6.999999999646178e-7 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_GraphCreation",
+            "value": 7.000000152856956e-7,
+            "unit": "ns/iter",
+            "extra": "iterations: 1000000000\ncpu: 6.999999999646178e-7 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_AddEdge",
+            "value": 1.681271785903104,
+            "unit": "ns/iter",
+            "extra": "iterations: 418184695\ncpu: 1.6765038256600953 ns\nthreads: 1"
           }
         ]
       }
