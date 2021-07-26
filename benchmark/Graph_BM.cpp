1e313f0c06ea8ca16d278292e6659faac03b35a3
ZigRazor <zigrazor@gmail.com>
2021-07-26 08:41:44 +0000

    Added Thread Safe Graph Benchmark

--- benchmark/Graph_BM.cpp
+++ benchmark/Graph_BM.cpp
@@ -48,13 +48,53 @@ static void AddEdgeX(benchmark::State &state)
 }
 BENCHMARK(AddEdgeX)->RangeMultiplier(16)->Range((unsigned long)1, (unsigned long)1 << 18);
 
-/*
-static void BM_AddEdgeX_MT(benchmark::State &state)
+static void GraphCreation_TS(benchmark::State &state)
+{
+    for (auto _ : state)
+    {
+        CXXGRAPH::Graph_TS<int> g;
+    }
+}
+
+BENCHMARK(GraphCreation_TS);
+
+static void AddEdge_TS(benchmark::State &state)
+{
+    CXXGRAPH::Graph_TS<int> g;
+    auto n1 = *nodes.at(0);
+    auto n2 = *nodes.at(1);
+    CXXGRAPH::Edge<int> e(1, n1, n2);
+    for (auto _ : state)
+    {
+        g.addEdge(&e);
+    }
+}
+BENCHMARK(AddEdge_TS);
+
+static void AddEdgeX_TS(benchmark::State &state)
+{
+    CXXGRAPH::Graph_TS<int> g;
+    auto range_start = edges.begin();
+    auto range_end = edges.find(state.range(0));
+    std::map<unsigned long, CXXGRAPH::Edge<int> *> edgesX;
+    edgesX.insert(range_start, range_end);
+    //std::cout << edges1k.size() << std::endl;
+    for (auto _ : state)
+    {
+        for (auto e : edgesX)
+        {
+            g.addEdge(&(*e.second));
+        }
+    }
+}
+BENCHMARK(AddEdgeX_TS)->RangeMultiplier(16)->Range((unsigned long)1, (unsigned long)1 << 18);
+
+static void BM_AddEdgeX_MT_TS(benchmark::State &state)
 {
     //std::cout << "Thread Number" << state.thread_index << std::endl;
     if (state.thread_index == 0)
     {
-        graph = new CXXGRAPH::Graph<int>();
+        graph = new CXXGRAPH::Graph_TS<int>();
     }
     auto subrange = state.range(0) / state.threads;
     //CXXGRAPH::Graph<int>* g = graph;
@@ -76,5 +116,4 @@ static void BM_AddEdgeX_MT(benchmark::State &state)
         delete graph;
     }
 }
-BENCHMARK(BM_AddEdgeX_MT)->RangeMultiplier(16)->Range((unsigned long)1 << 4, (unsigned long)1 << 18)->ThreadRange(1, 4);
-*/
+BENCHMARK(BM_AddEdgeX_MT_TS)->RangeMultiplier(16)->Range((unsigned long)1 << 4, (unsigned long)1 << 18)->ThreadRange(1, 4);
