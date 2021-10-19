0304eec651b9241e553f444a7a7e5020a46c0c3a
AlfredCP <82832709+AlfredCP@users.noreply.github.com>
2021-10-19 06:34:15 +0000

    Clean up temporary test files (#126)
    * Adding library name (CXXGRAPH) in header guards to avoid possible conflicts in include folders Node and Graph
    
    * Adding library name (CXXGRAPH) in header guards to avoid possible conflicts in include folders Edge, Partitioning and Utility
    
    * Added code to remove temporary teest files from build folder
    
    Co-authored-by: Parrot User <user@localhost.localdomain>

--- test/RWOutputTest.cpp
+++ test/RWOutputTest.cpp
@@ -63,6 +63,9 @@ TEST(RWOutputTest, test_1)
     ASSERT_TRUE(exists_test("graph.csv"));
     ASSERT_FALSE(exists_test("graph_NodeFeat.csv"));
     ASSERT_FALSE(exists_test("graph_EdgeWeight.csv"));
+
+    remove("graph.csv");
+    ASSERT_FALSE(exists_test("graph.csv"));
 }
 
 TEST(RWOutputTest, test_2)
@@ -84,6 +87,9 @@ TEST(RWOutputTest, test_2)
     ASSERT_TRUE(exists_test("graph.csv"));
     ASSERT_FALSE(exists_test("graph_NodeFeat.csv"));
     ASSERT_FALSE(exists_test("graph_EdgeWeight.csv"));
+
+    remove("graph.csv");
+    ASSERT_FALSE(exists_test("graph.csv"));
 }
 
 TEST(RWOutputTest, test_3)
@@ -105,6 +111,9 @@ TEST(RWOutputTest, test_3)
     ASSERT_TRUE(exists_test("test_3.csv"));
     ASSERT_FALSE(exists_test("test_3_NodeFeat.csv"));
     ASSERT_FALSE(exists_test("test_3_EdgeWeight.csv"));
+
+    remove("test_3.csv");
+    ASSERT_FALSE(exists_test("test_3.csv"));
 }
 
 TEST(RWOutputTest, test_4)
@@ -141,6 +150,13 @@ TEST(RWOutputTest, test_5)
     ASSERT_TRUE(exists_test("test_5.csv"));
     ASSERT_TRUE(exists_test("test_5_NodeFeat.csv"));
     ASSERT_TRUE(exists_test("test_5_EdgeWeight.csv"));
+
+    remove("test_5.csv");
+    remove("test_5_NodeFeat.csv");
+    remove("test_5_EdgeWeight.csv");
+    ASSERT_FALSE(exists_test("test_5.csv"));
+    ASSERT_FALSE(exists_test("test_5_NodeFeat.csv"));
+    ASSERT_FALSE(exists_test("test_5_EdgeWeight.csv"));
 }
 
 TEST(RWOutputTest, test_6)
@@ -159,6 +175,11 @@ TEST(RWOutputTest, test_6)
     ASSERT_TRUE(exists_test("test_6.csv"));
     ASSERT_FALSE(exists_test("test_6_NodeFeat.csv"));
     ASSERT_TRUE(exists_test("test_6_EdgeWeight.csv"));
+
+    remove("test_6.csv");
+    remove("test_6_EdgeWeight.csv");
+    ASSERT_FALSE(exists_test("test_6.csv"));
+    ASSERT_FALSE(exists_test("test_6_EdgeWeight.csv"));
 }
 
 TEST(RWOutputTest, test_7)
@@ -177,6 +198,11 @@ TEST(RWOutputTest, test_7)
     ASSERT_TRUE(exists_test("test_7.csv"));
     ASSERT_TRUE(exists_test("test_7_NodeFeat.csv"));
     ASSERT_FALSE(exists_test("test_7_EdgeWeight.csv"));
+
+    remove("test_7.csv");
+    remove("test_7_NodeFeat.csv");
+    ASSERT_FALSE(exists_test("test_7.csv"));
+    ASSERT_FALSE(exists_test("test_7_NodeFeat.csv"));
 }
 
 TEST(RWOutputTest, test_8)
@@ -237,6 +263,9 @@ TEST(RWOutputTest, test_8)
             ASSERT_TRUE(false); // forced Error
         }
     }
+
+    remove("test_8.csv");
+    ASSERT_FALSE(exists_test("test_8.csv"));
 }
 
 TEST(RWOutputTest, test_9)
@@ -297,6 +326,11 @@ TEST(RWOutputTest, test_9)
             ASSERT_TRUE(false); // forced Error
         }
     }
+
+    remove("test_9.csv");
+    remove("test_9_NodeFeat.csv");
+    ASSERT_FALSE(exists_test("test_9.csv"));
+    ASSERT_FALSE(exists_test("test_9_NodeFeat.csv"));
 }
 
 TEST(RWOutputTest, test_10)
@@ -362,6 +396,13 @@ TEST(RWOutputTest, test_10)
             ASSERT_TRUE(false); // forced Error
         }
     }
+
+    remove("test_10.csv");
+    remove("test_10_NodeFeat.csv");
+    remove("test_10_EdgeWeight.csv");
+    ASSERT_FALSE(exists_test("test_10.csv"));
+    ASSERT_FALSE(exists_test("test_10_NodeFeat.csv"));
+    ASSERT_FALSE(exists_test("test_10_EdgeWeight.csv"));
 }
 
 //************* TSV ***************//
@@ -383,8 +424,12 @@ TEST(RWOutputTest, test_11)
     int res = graph.writeToFile();
     ASSERT_EQ(res, 0);
     ASSERT_FALSE(exists_test("graph.tsv"));
+    ASSERT_TRUE(exists_test("graph.csv"));
     ASSERT_FALSE(exists_test("graph_NodeFeat.tsv"));
     ASSERT_FALSE(exists_test("graph_EdgeWeight.tsv"));
+
+    remove("graph.csv");
+    ASSERT_FALSE(exists_test("graph.csv"));
 }
 
 TEST(RWOutputTest, test_12)
@@ -406,6 +451,9 @@ TEST(RWOutputTest, test_12)
     ASSERT_TRUE(exists_test("graph.tsv"));
     ASSERT_FALSE(exists_test("graph_NodeFeat.tsv"));
     ASSERT_FALSE(exists_test("graph_EdgeWeight.tsv"));
+
+    remove("graph.tsv");
+    ASSERT_FALSE(exists_test("graph.tsv"));
 }
 
 TEST(RWOutputTest, test_13)
@@ -427,6 +475,9 @@ TEST(RWOutputTest, test_13)
     ASSERT_TRUE(exists_test("test_13.tsv"));
     ASSERT_FALSE(exists_test("test_13_NodeFeat.tsv"));
     ASSERT_FALSE(exists_test("test_13_EdgeWeight.tsv"));
+
+    remove("test_13.tsv");
+    ASSERT_FALSE(exists_test("test_13.tsv"));
 }
 
 TEST(RWOutputTest, test_14)
@@ -463,6 +514,13 @@ TEST(RWOutputTest, test_15)
     ASSERT_TRUE(exists_test("test_15.tsv"));
     ASSERT_TRUE(exists_test("test_15_NodeFeat.tsv"));
     ASSERT_TRUE(exists_test("test_15_EdgeWeight.tsv"));
+
+    remove("test_15.tsv");
+    remove("test_15_NodeFeat.tsv");
+    remove("test_15_EdgeWeight.tsv");
+    ASSERT_FALSE(exists_test("test_15.tsv"));
+    ASSERT_FALSE(exists_test("test_15_NodeFeat.tsv"));
+    ASSERT_FALSE(exists_test("test_15_EdgeWeight.tsv"));
 }
 
 TEST(RWOutputTest, test_16)
@@ -481,6 +539,11 @@ TEST(RWOutputTest, test_16)
     ASSERT_TRUE(exists_test("test_16.tsv"));
     ASSERT_FALSE(exists_test("test_16_NodeFeat.tsv"));
     ASSERT_TRUE(exists_test("test_16_EdgeWeight.tsv"));
+
+    remove("test_16.tsv");
+    remove("test_16_EdgeWeight.tsv");
+    ASSERT_FALSE(exists_test("test_16.tsv"));
+    ASSERT_FALSE(exists_test("test_16_EdgeWeight.tsv"));
 }
 
 TEST(RWOutputTest, test_17)
@@ -499,6 +562,11 @@ TEST(RWOutputTest, test_17)
     ASSERT_TRUE(exists_test("test_17.tsv"));
     ASSERT_TRUE(exists_test("test_17_NodeFeat.tsv"));
     ASSERT_FALSE(exists_test("test_17_EdgeWeight.tsv"));
+
+    remove("test_17.tsv");
+    remove("test_17_NodeFeat.tsv");
+    ASSERT_FALSE(exists_test("test_17.tsv"));
+    ASSERT_FALSE(exists_test("test_17_NodeFeat.tsv"));
 }
 
 TEST(RWOutputTest, test_18)
@@ -559,6 +627,9 @@ TEST(RWOutputTest, test_18)
             ASSERT_TRUE(false); // forced Error
         }
     }
+
+    remove("test_18.tsv");
+    ASSERT_FALSE(exists_test("test_18.tsv"));
 }
 
 TEST(RWOutputTest, test_19)
@@ -619,6 +690,11 @@ TEST(RWOutputTest, test_19)
             ASSERT_TRUE(false); // forced Error
         }
     }
+
+    remove("test_19.tsv");
+    remove("test_19_NodeFeat.tsv");
+    ASSERT_FALSE(exists_test("test_19.tsv"));
+    ASSERT_FALSE(exists_test("test_19_NodeFeat.tsv"));
 }
 
 TEST(RWOutputTest, test_20)
@@ -684,6 +760,13 @@ TEST(RWOutputTest, test_20)
             ASSERT_TRUE(false); // forced Error
         }
     }
+
+    remove("test_20.tsv");
+    remove("test_20_NodeFeat.tsv");
+    remove("test_20_EdgeWeight.tsv");
+    ASSERT_FALSE(exists_test("test_20.tsv"));
+    ASSERT_FALSE(exists_test("test_20_NodeFeat.tsv"));
+    ASSERT_FALSE(exists_test("test_20_EdgeWeight.tsv"));
 }
 
 TEST(RWOutputTest, test_21)
@@ -705,6 +788,11 @@ TEST(RWOutputTest, test_21)
     ASSERT_TRUE(exists_test("test_21.csv.gz"));
     ASSERT_TRUE(exists_test("test_21_NodeFeat.csv.gz"));
     ASSERT_FALSE(exists_test("test_21_EdgeWeight.csv.gz"));
+
+    remove("test_21.csv.gz");
+    remove("test_21_NodeFeat.csv.gz");
+    ASSERT_FALSE(exists_test("test_21.csv.gz"));
+    ASSERT_FALSE(exists_test("test_21_NodeFeat.csv.gz"));
 }
 
 TEST(RWOutputTest, test_22)
@@ -774,6 +862,19 @@ TEST(RWOutputTest, test_22)
             ASSERT_TRUE(false); // forced Error
         }
     }
+
+    remove("test_22.csv");
+    remove("test_22_NodeFeat.csv");
+    remove("test_22_EdgeWeight.csv");
+    remove("test_22.csv.gz");
+    remove("test_22_NodeFeat.csv.gz");
+    remove("test_22_EdgeWeight.csv.gz");
+    ASSERT_FALSE(exists_test("test_22.csv"));
+    ASSERT_FALSE(exists_test("test_22_NodeFeat.csv"));
+    ASSERT_FALSE(exists_test("test_22_EdgeWeight.csv"));
+    ASSERT_FALSE(exists_test("test_22.csv.gz"));
+    ASSERT_FALSE(exists_test("test_22_NodeFeat.csv.gz"));
+    ASSERT_FALSE(exists_test("test_22_EdgeWeight.csv.gz"));
 }
 
 TEST(RWOutputTest, test_23)
@@ -795,6 +896,9 @@ TEST(RWOutputTest, test_23)
     ASSERT_TRUE(exists_test("test_23.csv.gz"));
     ASSERT_FALSE(exists_test("test_23_NodeFeat.csv.gz"));
     ASSERT_FALSE(exists_test("test_23_EdgeWeight.csv.gz"));
+
+    remove("test_23.csv.gz");
+    ASSERT_FALSE(exists_test("test_23.csv.gz"));
 }
 
 TEST(RWOutputTest, test_24)
@@ -816,6 +920,11 @@ TEST(RWOutputTest, test_24)
     ASSERT_TRUE(exists_test("test_24.tsv.gz"));
     ASSERT_TRUE(exists_test("test_24_NodeFeat.tsv.gz"));
     ASSERT_FALSE(exists_test("test_24_EdgeWeight.tsv.gz"));
+
+    remove("test_24.tsv.gz");
+    remove("test_24_NodeFeat.tsv.gz");
+    ASSERT_FALSE(exists_test("test_24.tsv.gz"));
+    ASSERT_FALSE(exists_test("test_24_NodeFeat.tsv.gz"));
 }
 
 TEST(RWOutputTest, test_25)
@@ -885,6 +994,19 @@ TEST(RWOutputTest, test_25)
             ASSERT_TRUE(false); // forced Error
         }
     }
+
+    remove("test_25.tsv");
+    remove("test_25_NodeFeat.tsv");
+    remove("test_25_EdgeWeight.tsv");
+    remove("test_25.tsv.gz");
+    remove("test_25_NodeFeat.tsv.gz");
+    remove("test_25_EdgeWeight.tsv.gz");
+    ASSERT_FALSE(exists_test("test_25.tsv"));
+    ASSERT_FALSE(exists_test("test_25_NodeFeat.tsv"));
+    ASSERT_FALSE(exists_test("test_25_EdgeWeight.tsv"));
+    ASSERT_FALSE(exists_test("test_25.tsv.gz"));
+    ASSERT_FALSE(exists_test("test_25_NodeFeat.tsv.gz"));
+    ASSERT_FALSE(exists_test("test_25_EdgeWeight.tsv.gz"));
 }
 
 TEST(RWOutputTest, test_26)
@@ -906,6 +1028,9 @@ TEST(RWOutputTest, test_26)
     ASSERT_TRUE(exists_test("test_26.tsv.gz"));
     ASSERT_FALSE(exists_test("test_26_NodeFeat.tsv.gz"));
     ASSERT_FALSE(exists_test("test_26_EdgeWeight.tsv.gz"));
+    
+    remove("test_26.tsv.gz");
+    ASSERT_FALSE(exists_test("test_26.tsv.gz"));
 }
 
 TEST(RWOutputTest, test_27)
@@ -932,6 +1057,9 @@ TEST(RWOutputTest, test_27)
     fileCompressed.seekg(0, std::ios::end);
     int file_compressed_size = fileCompressed.tellg();
     ASSERT_LE(file_compressed_size, file_size);
+
+    remove("test_27.tsv.gz");
+    ASSERT_FALSE(exists_test("test_27.tsv.gz"));
 }
 
 
