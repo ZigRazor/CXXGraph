4381220dfee1d5b835c63994522a238a1e8fb8fc
Luigi Morel <morelmiles@gmail.com>
2022-04-03 18:31:18 +0300

    add project information

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -13,8 +13,8 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ZigRazor', // Usually your GitHub org/user name.
+  projectName: 'CXXGraph', // Usually your repo name.
 
   presets: [
     [
@@ -24,14 +24,12 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/ZigRazor/CXXGraph',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/ZigRazor/CXXGraph',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
