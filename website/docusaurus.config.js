2e1016885f0660671e86dccdac2118b009b5016e
Luigi Morel <morelmiles@gmail.com>
2022-04-05 21:13:10 +0300

    add the logo

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -45,7 +45,7 @@ const config = {
         title: 'CXXGraph',
         logo: {
           alt: 'CXXGraph Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
