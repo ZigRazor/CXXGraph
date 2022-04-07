451cb9bfcc0f9258c3cbb1085b9c4ab049508f8f
Luigi Morel <48476293+morelmiles@users.noreply.github.com>
2022-04-07 09:43:41 +0300

    Landing page for the website  (#203)
    * initial commit
    
    * initial commit
    
    * remove unused images
    
    * add assets
    
    * update the menu links
    
    * update cxxgraph features section
    
    * delete the blog folder
    
    * delete the blog links
    
    * Update the project's intro
    
    * customize hero section
    
    * add project information
    
    * remove the broken link
    
    * add the logo

---
+++ website/docs/intro.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+---
+
+# Introduction
+
+CXXGraph is a small library, header only, that manages the Graph and it's algorithms in C++. In other words a "Comprehensive C++ Graph Library". An alternative to [Boost Graph Library (BGL)](https://www.boost.org/doc/libs/1_77_0/libs/graph/doc/index.html)
---
+++ website/src/pages/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx(styles.banner__container, styles.heroBanner)}>
+      <div className={clsx('container', styles.header__container)}>
+        <h1 className={styles.title}>{siteConfig.title}</h1>
+        <p className={styles.subTitle}>{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className='button button--secondary button--lg'
+            to='/docs/intro'
+          >
+            Documentation 📚
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Home | ${siteConfig.title}`}
+      description=' A comprehensive C++ graph library'
+    >
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
