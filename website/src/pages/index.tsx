5312e03db5469dc90cae2bc93c04880ef3856664
Luigi Morel <morelmiles@gmail.com>
2022-04-20 08:52:06 +0300

    change intro page label

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className='button button--secondary button--lg'
-            to='/docs/intro'
+            to='/docs/Intro'
           >
             Documentation 📚
           </Link>
