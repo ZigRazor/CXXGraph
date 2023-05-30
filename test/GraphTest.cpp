cedbfbdb9a2ced9c6dfaae53134739e07d4f0be0
sbaldu <simone.balducci00@gmail.com>
2023-05-30 12:13:30 +0200

    Add tests for the out/inOutEdges methods

--- test/GraphTest.cpp
+++ test/GraphTest.cpp
@@ -1,3 +1,5 @@
+#include <set>
+
 #include "CXXGraph.hpp"
 #include "gtest/gtest.h"
 
@@ -225,4 +227,101 @@ TEST(GraphTest, set_data) {
   for (const auto& nodeIt : graph.getNodeSet()) {
 	ASSERT_EQ(nodeIt->getData(), data_values[nodeIt->getUserId()]);
   }
+}
+
+TEST(GraphTest, test_outEdges) {
+  CXXGraph::Node<int> node1("1", 1);
+  CXXGraph::Node<int> node2("2", 2);
+  CXXGraph::Node<int> node3("3", 3);
+  CXXGraph::Node<int> node4("4", 4);
+  CXXGraph::Node<int> node5("5", 5);
+  CXXGraph::Node<int> node6("6", 6);
+  CXXGraph::Node<int> node7("7", 7);
+  CXXGraph::Node<int> node8("8", 8);
+  CXXGraph::DirectedEdge<int> edge1(1, node1, node2);
+  CXXGraph::DirectedEdge<int> edge2(2, node1, node3);
+  CXXGraph::DirectedEdge<int> edge3(3, node2, node4);
+  CXXGraph::DirectedEdge<int> edge4(4, node2, node5);
+  CXXGraph::DirectedEdge<int> edge5(5, node3, node4);
+  CXXGraph::DirectedEdge<int> edge6(6, node3, node5);
+  CXXGraph::DirectedEdge<int> edge7(7, node4, node6);
+  CXXGraph::DirectedEdge<int> edge8(8, node4, node7);
+  CXXGraph::DirectedEdge<int> edge9(7, node5, node6);
+  CXXGraph::DirectedEdge<int> edge10(8, node5, node7);
+  CXXGraph::DirectedEdge<int> edge11(8, node6, node8);
+  CXXGraph::DirectedEdge<int> edge12(8, node7, node8);
+  CXXGraph::T_EdgeSet<int> edgeSet;
+  edgeSet.insert(&edge1);
+  edgeSet.insert(&edge2);
+  edgeSet.insert(&edge3);
+  edgeSet.insert(&edge4);
+  edgeSet.insert(&edge5);
+  edgeSet.insert(&edge6);
+  edgeSet.insert(&edge7);
+  edgeSet.insert(&edge8);
+  edgeSet.insert(&edge9);
+  edgeSet.insert(&edge10);
+  edgeSet.insert(&edge11);
+  edgeSet.insert(&edge12);
+  CXXGraph::Graph<int> graph(edgeSet);
+
+  // Check node 1
+  for (auto x : graph.outEdges(&node1)) {
+	ASSERT_TRUE(x == &node2 || x == &node3);
+  } 
+  // Check node 2
+  for (auto x : graph.outEdges(&node2)) {
+	ASSERT_TRUE(x == &node4 || x == &node5);
+	ASSERT_FALSE(x == &node1);
+  } 
+  // Check node 5
+  for (auto x : graph.outEdges(&node5)) {
+	ASSERT_TRUE(x == &node6 || x == &node7);
+	ASSERT_FALSE(x == &node2);
+	ASSERT_FALSE(x == &node3);
+  } 
+  // Check that node 8 does not have any neighbors
+  ASSERT_EQ(graph.outEdges(&node8), std::set<const CXXGraph::Node<int> *>());
+}
+
+TEST(GraphTest, test_inOutEdges) {
+  CXXGraph::Node<int> node1("1", 1);
+  CXXGraph::Node<int> node2("2", 2);
+  CXXGraph::Node<int> node3("3", 3);
+  CXXGraph::Node<int> node4("4", 4);
+  CXXGraph::Node<int> node5("5", 5);
+  CXXGraph::Node<int> node6("6", 6);
+  CXXGraph::Node<int> node7("7", 7);
+  CXXGraph::Node<int> node8("8", 8);
+  CXXGraph::UndirectedEdge<int> edge1(1, node1, node2);
+  CXXGraph::UndirectedEdge<int> edge2(2, node1, node3);
+  CXXGraph::UndirectedEdge<int> edge3(3, node2, node3);
+  CXXGraph::UndirectedEdge<int> edge4(4, node2, node4);
+  CXXGraph::UndirectedEdge<int> edge5(5, node4, node5);
+  CXXGraph::UndirectedEdge<int> edge6(6, node4, node6);
+  CXXGraph::UndirectedEdge<int> edge7(7, node6, node7);
+  CXXGraph::UndirectedEdge<int> edge8(8, node6, node8);
+  CXXGraph::T_EdgeSet<int> edgeSet;
+  edgeSet.insert(&edge1);
+  edgeSet.insert(&edge2);
+  edgeSet.insert(&edge3);
+  edgeSet.insert(&edge4);
+  edgeSet.insert(&edge5);
+  edgeSet.insert(&edge6);
+  edgeSet.insert(&edge7);
+  edgeSet.insert(&edge8);
+  CXXGraph::Graph<int> graph(edgeSet);
+
+  // Check node 1
+  for (auto x : graph.inOutEdges(&node1)) {
+	ASSERT_TRUE(x == &node2 || x == &node3);
+  } 
+  // Check node 4
+  for (auto x : graph.inOutEdges(&node4)) {
+	ASSERT_TRUE(x == &node2 || x == &node3 || x == &node5 || x == &node6);
+  } 
+  // Check node 7
+  for (auto x : graph.inOutEdges(&node7)) {
+	ASSERT_TRUE(x == &node6);
+  } 
 }
