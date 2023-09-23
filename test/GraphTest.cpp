6b853032cbf68b6ca01d72808307058bc7c5c8b1
guru2396 <gururajm2396@gmail.com>
2023-09-23 16:25:28 +0530

    added more tests for findEdge()

--- test/GraphTest.cpp
+++ test/GraphTest.cpp
@@ -128,8 +128,14 @@ TEST(GraphTest, FindEdge_Test) {
   ASSERT_TRUE(graph.findEdge(&node1,&node5,edgeId));
   ASSERT_FALSE(graph.findEdge(&node5,&node1,edgeId));
   graph.removeEdge(2);
+  CXXGraph::UndirectedWeightedEdge<int> edge5(7, node4, node5, 5);
+  CXXGraph::DirectedWeightedEdge<int> edge6(8, node2, node5, 10);
+  graph.addEdge(make_shared<CXXGraph::UndirectedWeightedEdge<int>>(edge5));
+  graph.addEdge(make_shared<CXXGraph::DirectedWeightedEdge<int>>(edge6));
   ASSERT_FALSE(graph.findEdge(&node2,&node3,edgeId));
   ASSERT_FALSE(graph.findEdge(&node3,&node2,edgeId));
+  CXXGraph::Graph<int> graph2;
+  ASSERT_FALSE(graph2.findEdge(&node2,&node3,edgeId));
 }
 
 TEST(GraphTest, RawAddEdge_1) {
