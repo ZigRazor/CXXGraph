8c8075785743c3f9a075fa62db2ba5272b7768db
guru2396 <88192735+guru2396@users.noreply.github.com>
2023-09-25 12:37:05 +0530

    Caching Adjacency Matrix for faster edge lookup (#344)
    * Added changes to save nodes in Dijkstra shortest path. Modified tests.
    
    * Caching adjacency matrix for faster edge lookup
    
    * changing return type of getCacheAdjMatrix(), using alias for shared_ptr
    
    * using alias for shared_ptr, removing unused vars
    
    * added more tests for findEdge()
    
    * added more tests for findEdge()
    
    * renaming function, adding spaces

--- test/GraphTest.cpp
+++ test/GraphTest.cpp
@@ -105,6 +105,54 @@ TEST(GraphTest, GetNodeSet_2) {
               }) != nodeSet.end());
 }
 
+TEST(GraphTest, FindEdge_Test) {
+  CXXGraph::Node<int> node1("1", 1);
+  CXXGraph::Node<int> node2("2", 2);
+  CXXGraph::Node<int> node3("3", 3);
+  CXXGraph::Node<int> node4("4", 2);
+  CXXGraph::Node<int> node5("5", 3);
+  CXXGraph::UndirectedEdge<int> edge(1, node1, node2);
+  CXXGraph::UndirectedEdge<int> edge2(2, node2, node3);
+  CXXGraph::T_EdgeSet<int> edgeSet;
+
+  /* adding edges to the edgeset */
+
+  edgeSet.insert(make_shared<CXXGraph::UndirectedEdge<int>>(edge));
+  edgeSet.insert(make_shared<CXXGraph::UndirectedEdge<int>>(edge2));
+  CXXGraph::Graph<int> graph(edgeSet);
+  unsigned long long edgeId = 0;
+  ASSERT_TRUE(graph.findEdge(&node1,&node2,edgeId));
+  CXXGraph::UndirectedEdge<int> edge3(3, node1, node3);
+
+  /* adding edge using addEdge() */
+
+  graph.addEdge(make_shared<CXXGraph::UndirectedEdge<int>>(edge3));
+  ASSERT_TRUE(graph.findEdge(&node1,&node3,edgeId));
+  ASSERT_TRUE(graph.findEdge(&node3,&node1,edgeId));
+  CXXGraph::DirectedEdge<int> edge4(4, node1, node5);
+  graph.addEdge(make_shared<CXXGraph::DirectedEdge<int>>(edge4));
+  ASSERT_TRUE(graph.findEdge(&node1,&node5,edgeId));
+  ASSERT_FALSE(graph.findEdge(&node5,&node1,edgeId));
+
+  /* removing edge using removeEdge() */
+
+  graph.removeEdge(2);
+  CXXGraph::UndirectedWeightedEdge<int> edge5(7, node4, node5, 5);
+  CXXGraph::DirectedWeightedEdge<int> edge6(8, node2, node5, 10);
+
+  /* adding edge using addEdge() */
+  
+  graph.addEdge(&edge5);
+  graph.addEdge(&edge6);
+  ASSERT_FALSE(graph.findEdge(&node2,&node3,edgeId));
+  ASSERT_FALSE(graph.findEdge(&node3,&node2,edgeId));
+
+  /* Test for empty graph */
+
+  CXXGraph::Graph<int> graph2;
+  ASSERT_FALSE(graph2.findEdge(&node2,&node3,edgeId));
+}
+
 TEST(GraphTest, RawAddEdge_1) {
   CXXGraph::Node<int> n1("a", 1);
   CXXGraph::Node<int> n2("b", 1);
