1b31e6307dbe89b622fd719021fb96db96c78e44
Jasmine Tang <tanghocle456@gmail.com>
2024-02-01 22:52:38 -0800

    Welsh powell coloring (#395)
    * Wokring on getNodeSet, breaking tests
    
    * Added welshPowellColoring algorithm, breaking 1 test in welshPowerColoringTest.cpp
    
    * Removing assertions and <cassert>
    
    * Transitioned from shared_ptr to node<T>, all tests passed
    
    * Beautify code and fix comments in code
    
    * Sync from fork
    
    * WelshPowellColoring: Bypass map in constructing degree of vertex and go straight through vector
    
    * WelshPowellColoring: Enforce const in for loops
    
    * Welsh Powell: Move std::vector creation out of loop, opting for std solution instead of for loops
    
    * Add documentation for welsh powell coloring algo

--- include/CXXGraph/Graph/Algorithm/welshPowellColoring_impl.hpp
+++ include/CXXGraph/Graph/Algorithm/welshPowellColoring_impl.hpp
@@ -13,43 +13,36 @@ namespace CXXGraph {
 template <typename T>
 std::map<Node<T>, int> Graph<T>::welshPowellColoring() const {
   auto adjMatrix = *getAdjMatrix();
-  std::unordered_map<std::shared_ptr<const Node<T>>, int> degreeOfVertexMap = {};
 
-  // Find the degree of each vertex and put them in a map
-  for (auto &[nodeFrom, nodeToEdgeVec] : adjMatrix) {
-    degreeOfVertexMap[nodeFrom] = nodeToEdgeVec.size();
+  std::vector<std::pair<std::shared_ptr<const Node<T>>, int>> degreeOfVertexVector = {};
+  // Find the degree of each vertex and put them in a vector
+  for (const auto &[nodeFrom, nodeToEdgeVec] : adjMatrix) {
+    degreeOfVertexVector.push_back({nodeFrom, nodeToEdgeVec.size()});
   }
-
-  // Transform the map to the vector to sort
-  std::vector<std::pair<std::shared_ptr<const Node<T>>, int>> degreeOfVertexVector(degreeOfVertexMap.begin(), degreeOfVertexMap.end());
 
   // Sort them based on the vertex degree
   std::sort(degreeOfVertexVector.begin(), degreeOfVertexVector.end(), [](const auto& left, const auto& right) {
     return left.second > right.second;
   });
 
-  // Create a new map of coloring, where the keys are the nodes, and the value is the color order (assigned by integer)
+  // Create a new map of coloring, where the keys a	re the nodes, and the value is the color order (assigned by integer)
   std::map<Node<T>, int> mapOfColoring;
-  for (auto &[nodeFrom, _] : adjMatrix) {
+  for (const auto &[nodeFrom, _] : adjMatrix) {
     mapOfColoring[*nodeFrom] = 0;
   }
 
   // Going down the list of vertex based on degrees
+  std::vector<int> usedColors(degreeOfVertexVector.size() + 1, 0);
   for (const auto& [node, _] : degreeOfVertexVector) {
     // Find the smallest unused color for the current vertex
-    std::vector<int> usedColors(degreeOfVertexVector.size() + 1, 0);
-
+	std::fill(usedColors.begin(), usedColors.end(), 0);
     for (const auto &[neighbor, _] : adjMatrix[node]) {
       usedColors[mapOfColoring[*neighbor]] = 1;
     }
 
     // Assign the smallest unused color to the current vertex
-    for (int c = 1; c < usedColors.size(); c++) {
-      if (usedColors[c] == 0) {
-        mapOfColoring[*node] = c;
-        break;
-      }
-    }
+	auto it = std::find(usedColors.begin() + 1, usedColors.end(), 0);
+	mapOfColoring[*node] = std::distance(usedColors.begin(), it);
   }
 
   return mapOfColoring;
