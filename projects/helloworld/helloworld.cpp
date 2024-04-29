7b0f24359ba19aa27429b69360c7dacaf2dc53d5
Naudeyboo <53095360+Horicabu@users.noreply.github.com>
2024-04-28 21:43:04 -0700

    add native getting started guide

---
+++ projects/helloworld/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y;
+  int sum;
+  cout << "Type a number: ";
+  cin >> x;
+  cout << "Type another number: ";
+  cin >> y;
+  sum = x + y;
+  cout << "Sum is: " << sum; 
+} 
