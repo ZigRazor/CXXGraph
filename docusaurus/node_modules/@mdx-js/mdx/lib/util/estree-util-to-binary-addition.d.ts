8d80d2d0eec874f88ed0e06428ad5107e4fc1433
ZigRazor <matteobotticci@gmail.com>
2024-04-29 08:55:15 +0200

    Revert "Add Native Getting Started Guide (#420)"
    This reverts commit 339301d434880ba40994a78597006cc066141c78.

--- docusaurus/node_modules/@mdx-js/mdx/lib/util/estree-util-to-binary-addition.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @param {ReadonlyArray<Expression>} expressions
- *   Expressions.
- * @returns {Expression}
- *   Addition.
- */
-export function toBinaryAddition(expressions: ReadonlyArray<Expression>): Expression;
-export type Expression = import('estree-jsx').Expression;
-//# sourceMappingURL=estree-util-to-binary-addition.d.ts.map
