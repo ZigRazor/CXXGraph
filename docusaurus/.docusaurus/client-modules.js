9a8536c48eceda7cf0b94ddd87f0009e77de9c49
ZigRazor <zigrazor@gmail.com>
2024-04-29 08:55:58 +0200

    Revert "Add Native Getting Started Guide (#420)" (#421)
    This reverts commit 339301d434880ba40994a78597006cc066141c78.

--- docusaurus/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/horicabu/docusaurus-repo/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/horicabu/docusaurus-repo/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/horicabu/docusaurus-repo/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/horicabu/docusaurus-repo/src/css/custom.css"),
-];
