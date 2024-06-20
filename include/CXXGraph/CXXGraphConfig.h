0933dad66b5961c3c914d38b340d6f0e67c9467d
ZigRazor <zigrazor@gmail.com>
2024-06-20 09:23:10 +0200

    Update CXXGraphConfig.h

--- include/CXXGraph/CXXGraphConfig.h
+++ include/CXXGraph/CXXGraphConfig.h
@@ -1,4 +1,4 @@
 // the configured options and settings for CXXGraph
-#define CXXGraph_VERSION_MAJOR 3
-#define CXXGraph_VERSION_MINOR 1
+#define CXXGraph_VERSION_MAJOR 4
+#define CXXGraph_VERSION_MINOR 0
 #define CXXGraph_VERSION_PATCH 0
