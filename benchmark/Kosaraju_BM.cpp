5cc3a6edec99b6101c46e714bd100f6de652e852
daniel <daniel.a.radzikrahman@outlook.com>
2024-10-09 16:58:07 +0100

    Kosaraju benchmarks added

---
+++ benchmark/Kosaraju_BM.cpp
@@ -0,0 +1,30 @@
+#include <benchmark/benchmark.h>
+
+#include "CXXGraph/CXXGraph.hpp"
+#include "Utilities.hpp"
+
+static void Kosaraju_X(benchmark::State &state) {
+  CXXGraph::Graph<int> g;
+  auto range_start = edges.begin();
+  auto range_end = edges.find(state.range(0));
+  std::unordered_map<unsigned long, CXXGraph::Edge<int> *> edgesX;
+  edgesX.insert(range_start, range_end);
+  for (auto e : edgesX) {
+    g.addEdge(&(*e.second));
+  }
+  for (auto _ : state) {
+    const auto &result = g.kosaraju();
+  }
+}
+BENCHMARK(Kosaraju_X)
+    ->RangeMultiplier(16)
+    ->Range((unsigned long)1, (unsigned long)1 << 16);
+
+static void Kosaraju_FromReadedCitHep(benchmark::State &state) {
+  auto edgeSet = cit_graph_ptr->getEdgeSet();
+  for (auto _ : state) {
+    const auto &result = cit_graph_ptr->kosaraju();
+  }
+}
+
+BENCHMARK(Kosaraju_FromReadedCitHep);
