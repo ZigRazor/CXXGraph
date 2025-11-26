ZigRazor/CXXGraph

commit 8f0544258a7d67d6c2ace203299f0f587b14495d

Resolve `modernize-deprecated-headers` (#555)

include/CXXGraph/Graph/Algorithm/BestFirstSearch_impl.hpp
@@ -107,6 +107,7 @@ BestFirstSearchResult<T> Graph<T>::best_first_search(
 template <typename T>
 const std::vector<Node<T>> Graph<T>::concurrency_breadth_first_search(
     const Node<T> &start, size_t num_threads) const {
+  num_threads = 1;
   std::vector<Node<T>> bfs_result;
   // check is exist node in the graph
   auto &nodeSet = Graph<T>::getNodeSet();

benchmark/Utilities.hpp
@@ -1,7 +1,6 @@
 #ifndef __UTILITIES_H__
 #define __UTILITIES_H__
-#include <time.h>
-
+#include <ctime>
 #include <random>
 
 #include "CXXGraph/CXXGraph.hpp"

examples/DialExample/dial_example.cpp
@@ -1,6 +1,5 @@
-#include <math.h>
-
 #include <CXXGraph/CXXGraph.hpp>
+#include <cmath>
 #include <memory>
 
 using std::make_shared;

examples/PartitionExample/partition_example.cpp
@@ -1,6 +1,5 @@
-#include <stdlib.h>
-#include <time.h>
-
+#include <cstdlib>
+#include <ctime>
 #include <iterator>
 
 #include "CXXGraph/CXXGraph.hpp"

test/Utilities.hpp
@@ -1,7 +1,6 @@
 #ifndef __UTILITIES_H__
 #define __UTILITIES_H__
-#include <time.h>
-
+#include <ctime>
 #include <memory>
 #include <random>
 

commit ff1e69d980e176ed2b00fe7addb280b40aa2c97b

Do not set `num_threads` to 1
This fixes an error introduced in
8f0544258a7d67d6c2ace203299f0f587b14495d
(cf. #555)

include/CXXGraph/Graph/Algorithm/BestFirstSearch_impl.hpp
@@ -112,7 +112,6 @@ BestFirstSearchResult<T> Graph<T>::best_first_search(
 template <typename T>
 const std::vector<Node<T>> Graph<T>::concurrency_breadth_first_search(
     const Node<T> &start, size_t num_threads) const {
-  num_threads = 1;
   std::vector<Node<T>> bfs_result;
   // check is exist node in the graph
   auto &nodeSet = Graph<T>::getNodeSet();